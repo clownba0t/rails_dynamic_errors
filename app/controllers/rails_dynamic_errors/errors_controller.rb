df617d0a70852c50964a8c8b122f60818b27106d
Daniel Carter <clownba0t@gmail.com>
2014-06-20 20:15:42 +0900

    Reworked to be generic - not tied to HTTP status codes any more Reordered methods to be more logical Added method to determine HTTP status code for an error (500 if can't determine)

--- app/controllers/rails_dynamic_errors/errors_controller.rb
+++ app/controllers/rails_dynamic_errors/errors_controller.rb
@@ -1,7 +1,9 @@
+require 'action_dispatch/middleware/exception_wrapper'
+
 module RailsDynamicErrors
   class ErrorsController < ApplicationController
-    helper_method :status_code
-    helper_method :status_name
+    helper_method :error_code
+    helper_method :error_name
     helper_method :error_message
   
     def show
@@ -9,20 +11,18 @@ module RailsDynamicErrors
     end
   
     private
-      def layout
-        template_exists?('errors', 'layouts') ? 'errors' : 'application'
-      end
-  
-      def template
-        template_exists?(status_code, 'rails_dynamic_errors/errors') ? status_code : 'show'
-      end
-  
-      def status_code
-        params[:code]
+      def error_code
+        # The route that leads to this controller is set up with a glob that
+        # maps to params[:code]. Unfortunately, because Rails memoizes request
+        # parameters, this mapped data is not available if we arrive in this
+        # controller after processing a request to a valid route but with a
+        # non-existent resource. Thankfully we can just take the same steps
+        # here as the glob anyway - extract the code from the path.
+        @error_code ||= env['PATH_INFO'][1..-1]
       end
 
-      def status_name
-        @status_name ||= Rack::Utils::HTTP_STATUS_CODES.fetch(status_code.to_i, 'Error')
+      def error_name
+        @error_name ||= Rack::Utils::HTTP_STATUS_CODES.fetch(status_code.to_i, 'Internal Server Error')
       end
   
       def error_message
@@ -35,6 +35,18 @@ module RailsDynamicErrors
   
       def exception
         @exception ||= env['action_dispatch.exception']
+      end
+
+      def status_code
+        @status_code ||= (exception) ? ActionDispatch::ExceptionWrapper.new(env, exception).status_code.to_s : '500'
+      end
+
+      def layout
+        template_exists?('errors', 'layouts') ? 'errors' : 'application'
+      end
+  
+      def template
+        template_exists?(error_code, 'rails_dynamic_errors/errors') ? error_code : 'show'
       end
   end
 end

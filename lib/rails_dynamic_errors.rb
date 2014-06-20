72e4400a87f6dc53a4f4eaadef5f90eea3c64e00
Daniel Carter <clownba0t@gmail.com>
2014-06-20 20:20:56 +0900

    Added install generator to base library file requires

--- lib/rails_dynamic_errors.rb
+++ lib/rails_dynamic_errors.rb
@@ -1,5 +1,6 @@
 require 'rails_dynamic_errors/middleware/dynamic_errors'
 require 'rails_dynamic_errors/engine'
+require 'generators/rails_dynamic_errors/install_generator'
 require 'active_support/dependencies'
 
 module RailsDynamicErrors
